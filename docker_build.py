ba735423f45f01999180d7e367a5b34869977a15
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-11-11 08:34:05 +0000

    Updated build script.

--- docker_build.py
+++ docker_build.py
@@ -4,6 +4,7 @@
 import getpass
 import os
 import sys
+import pathlib
 
 if not getpass.getuser() == "test_user":
 	raise NameError("Refusing to run when outside of a Docker container")
@@ -12,6 +13,8 @@ ANACONDA_TOKEN = os.environ.get("ANACONDA_TOKEN", '')
 
 if not ANACONDA_TOKEN:
 	raise ValueError("ANACONDA_TOKEN unset.")
+
+output_dir = pathlib.Path("/home/test_user/conda-bld/noarch")
 
 # Not writeable in Anaconda's own docker image 🙄
 os.system("sudo chown -R test_user: /opt/conda/pkgs")
@@ -32,5 +35,17 @@ for recipe in sys.argv[1:]:
 		command += f"-c {channel} "
 
 	os.system(command)
+
+	for filename in output_dir.glob(f"{recipe}-*.tar.bz2"):
+		print(filename)
+		print("Deploying to Anaconda.org...")
+		ret = os.system(f"anaconda - t $ANACONDA_TOKEN upload {filename}")
+		if ret == 0:
+			"Successfully deployed to Anaconda.org."
+			break
+		else:
+			raise OSError(f"Anaconda upload exited with non-zero code {ret}")
+	else:
+		raise FileNotFoundError(f"No matching files for {output_dir / f'{recipe}-*.tar.bz2'}")
 
 	os.chdir(root_dir)
